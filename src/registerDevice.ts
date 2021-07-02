516f21e14059205d3f2ac0ea124a97e19f5a4660
Antegral <antegral@antegral.net>
2021-07-02 19:00:46 +0900

    Fix account.config.json write condition logic Fix register function logic

--- src/registerDevice.ts
+++ src/registerDevice.ts
@@ -2,7 +2,7 @@ import * as config from "../config/account.config.json";
 import { accountConfig } from "../@types/account.config";
 import { accountData } from "../@types/registerDevice";
 import prompts from "prompts";
-import { writeFileSync } from "fs";
+import { writeFile } from "fs";
 import { join } from "path";
 import { util, AuthApiClient } from "node-kakao";
 import { logger } from "./logger";
@@ -41,15 +41,21 @@ export default class registerDevice {
       message: "클라이언트의 이름을 입력해주세요.",
     })
       .then((value) => {
-        logger.info("selected client name: " + value.clientName);
+        if (!value.clientName) {
+          logger.error("must fill client name!");
+        } else {
+          logger.info("selected client name: " + value.clientName);
 
-        return {
-          RegisteredDeviceName: value.clientName,
-          RegisteredUUID: util.randomWin32DeviceUUID(),
-        };
+          return {
+            RegisteredDeviceName: value.clientName,
+            RegisteredUUID: util.randomWin32DeviceUUID(),
+          };
+        }
       })
       .then((registeredDeviceData) => {
-        let configData = {
+        // reassign data logic //
+
+        this.configData = {
           AUTO_RETRY_CONNECT: config.AUTO_RETRY_CONNECT,
 
           KAKAO_ACCOUNT: {
@@ -59,17 +65,19 @@ export default class registerDevice {
 
           RegisteredDeviceName: registeredDeviceData.RegisteredDeviceName,
           RegisteredUUID: registeredDeviceData.RegisteredUUID,
-        };
-        // let assignedData = Object.assign(configData, registeredDeviceData);
+        }; // reassign data (internal)
 
-        writeFileSync(
+        /* writeFileSync(
           join(__dirname, "../config/account.config.json"),
           JSON.stringify(configData)
-        );
+        ); */ // reassign data (JSON)
 
-        logger.info("config assign completed.");
+        logger.info("client data reassigned. (internal)");
 
-        return configData;
+        return true;
+      })
+      .catch(() => {
+        throw new Error();
       });
   }
 
@@ -83,24 +91,42 @@ export default class registerDevice {
         this.configData.RegisteredUUID
       );
 
-      let response = await prompts({
-        type: "text",
-        name: "authCode",
-        message: "카카오톡으로 전송된 기기 인증번호를 입력해주세요.",
-        validate: (value) =>
-          0 <= parseInt(value) && parseInt(value) <= 9999
-            ? true
-            : `인증번호는 4자리의 숫자 입니다!`,
-      });
+      await client
+        .requestPasscode(this.accountData)
+        .then(() => logger.info("Auth Code sended.")); // Request Auth Code
 
-      let passcode = response.authCode as string;
+      // Auth Code User input
+      let passcode = (
+        await prompts({
+          type: "text",
+          name: "authCode",
+          message: "카카오톡으로 전송된 기기 인증번호를 입력해주세요.",
+          validate: (value) =>
+            0 <= parseInt(value) && parseInt(value) <= 9999
+              ? true
+              : `인증번호는 4자리의 숫자 입니다!`,
+        })
+      ).authCode as string; // authCode: any > authCode: string
 
-      return (await client.registerDevice(this.accountData, passcode, true))
-        .status;
+      let registerResult = (
+        await client.registerDevice(this.accountData, passcode, true)
+      )?.success;
+
+      if (registerResult) {
+        writeFile(
+          join(__dirname, "../config/account.config.json"),
+          JSON.stringify(this.configData),
+          () => logger.info("client data reassigned. (config.json)")
+        );
+
+        return true;
+      } else if (passcode !== undefined) {
+        logger.warn("AuthCode authentication Failed. retry register process.");
+        this.register(); // retry register
+      }
     } else {
-      this.generateDevice().then(() => {
-        this.register();
-      });
+      logger.error("register undefined device data!");
+      return null;
     }
   }
 }
