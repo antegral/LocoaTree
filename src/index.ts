af0dfb370e8b152f583aad0477149e64912dd837
antegral <antegral@antegral.net>
2022-11-27 21:41:02 +0900

    Update: Change Core library location

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
-import { VERSION } from "./runinfo.json";
-import prompts from "prompts";
-import registerDevice from "./registerDevice";
-import { logger } from "./logger";
-import LocoaTreeCore from "./core/index.core";
+import { VERSION } from './runinfo.json';
+import prompts from 'prompts';
+import registerDevice from './TalkClient';
+import { logger } from './logger';
+import LocoaTreeCore from './Core';
 
-export default class LocoaTree {
-  public regDevice: registerDevice;
+export default class Main {
+	public regDevice: registerDevice;
 
-  constructor() {
-    console.log("LocoaTree, KakaoTalk Bot Framework.");
-    this.readVersion();
-    this.regDevice = new registerDevice();
-    this.start();
-  }
+	constructor() {
+		console.log('LocoaTree, KakaoTalk Bot Framework.');
+		this.readVersion();
+		this.regDevice = new registerDevice();
+		this.start();
+	}
 
-  start() {
-    this.regDevice.check().then(async (loginResult) => {
-      if (loginResult?.status) {
-        new LocoaTreeCore();
-      } else {
-        await this.regDevice.generateDevice();
-        await this.regDevice.register();
-      }
-    });
-  }
+	start() {
+		this.regDevice.check().then(async (loginResult) => {
+			if (loginResult?.status) {
+				new LocoaTreeCore();
+			} else {
+				await this.regDevice.generateDevice();
+				await this.regDevice.register();
+			}
+		});
+	}
 
-  shutdown() {
-    throw new String("Shutdown LocoaTree.");
-  }
+	shutdown() {
+		throw new String('Shutdown LocoaTree.');
+	}
 
-  readVersion() {
-    VERSION
-      ? console.log(`VERSION > ${VERSION}`)
-      : () => {
-          throw new Error(
-            "버전을 읽는 중에 오류가 발생했습니다. 재설치 하세요."
-          );
-        };
-  }
+	readVersion() {
+		VERSION
+			? console.log(`VERSION > ${VERSION}`)
+			: () => {
+					throw new Error(
+						'버전을 읽는 중에 오류가 발생했습니다. 재설치 하세요.',
+					);
+			  };
+	}
 }
