1b5a2e196b761bc3817c4556b2d907394c45dbed
antegral <antegral@antegral.net>
2022-11-27 21:33:09 +0900

    Add: internal.ts type definition

---
+++ @types/Core/internal.d.ts
@@ -0,0 +1,8 @@
+export type { Schema } from './Internal/Config';
+export type { LogLevel } from './Internal/Log';
+export type {
+	LoginInfo,
+	KakaoAccount,
+	Device,
+	LoginOption,
+} from './Internal/TalkClient';
